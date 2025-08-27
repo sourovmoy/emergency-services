fd3d5452d02e7058912f303706b5b609659bb046
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-08-27 16:08:53 +0600

    creation call history section

--- practice.js
+++ practice.js
@@ -4,21 +4,5 @@ document
     alert("📞Calling National Emergency Service 999....");
     coinsCheck();
   });
-function strToNUm(id) {
-  const love = parseInt(document.getElementById(id).innerText);
-
-  return love;
-}
-
-function coinsCheck() {
-  const cutCoin = 20;
-  const coins = strToNUm("coins");
-  if (coins >= 20) {
-    const presentCoins = coins - cutCoin;
-    document.getElementById("coins").innerText = presentCoins;
-  } else {
-    return alert("❌Insufficient Coins");
-  }
-}
 
 // console.log(presentCoins);
